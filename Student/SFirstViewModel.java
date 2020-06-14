04f481b6d1429f76421332f05bb5afd5b01f96f5
MrLee12138 <1591426046@qq.com>
2020-06-14 17:17:54 +0800

    人脸识别及注册

--- Student/SFirstViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.bottomdemo.Student;
-
-import androidx.lifecycle.ViewModel;
-
-public class SFirstViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
--- Teacher/TFirstViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.bottomdemo;
-
-import androidx.lifecycle.ViewModel;
-
-public class TFirstViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
